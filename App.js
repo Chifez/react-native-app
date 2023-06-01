669553d008dd6d507e1a404db252cd62662776cc
emmanuel <chifez1@gmail.com>
2023-06-01 23:59:43 +0100

    made my first react native build

--- App.js
+++ App.js
@@ -2,14 +2,25 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, TextInput } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
 import Delete from "./pages/Delete";
 import Newnote from "./pages/Newnote";
 import Readnote from "./pages/Readnote";
 
+// SplashScreen.preventAutoHideAsync();
+
 export default function App() {
+  const [isLoaded] = useFonts({
+    otama: require("./assets/font/Otama-ep.otf"),
+  });
+
+  if (!isLoaded) return null;
+
   const Stack = createNativeStackNavigator();
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
@@ -24,6 +35,23 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {},
-});
+// import { StatusBar } from "expo-status-bar";
+// import { StyleSheet, Text, View } from "react-native";
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your apppp!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
--- pages/Home/index.jsx
+++ pages/Home/index.jsx
@@ -1,16 +1,41 @@
-import { Text, View, Button } from "react-native";
+import { Text, View, Button, Image, StyleSheet } from "react-native";
 
 const Home = ({ navigation }) => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <Image source={require("../../assets/svg.png")} />
+      <Text style={styles.Maintext}>All thougths one place</Text>
+      <Text style={styles.SubText}>
+        Dive right in and clear that mind of yours by writing your thougts down
+      </Text>
       <Button
-        title=" go to Dashboard"
+        title="go to next page"
         onPress={() => navigation.navigate("Dashboard")}
       />
-      <Button title="go back" onPress={() => navigation.goBack()} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    height: "100%",
+    flex: 1,
+  },
+  image: {},
+  Maintext: {
+    fontFamily: "otama",
+    fontSize: 60,
+    textAlign: "center",
+    lineHeight: 58,
+    marginTop: 45,
+    marginBottom: 25,
+  },
+  SubText: {
+    color: "#686868",
+    textAlign: "center",
+    fontSize: 16,
+    lineHeight: 22,
+  },
+});
 
 export default Home;
